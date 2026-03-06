29116ff2a5db6775ff70dd120da71f84ff1119ed
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2026-03-05 21:29:39 -0800

    added code for playing audio after an item is collected

--- Assets/Scripts/CollectObject.cs
+++ Assets/Scripts/CollectObject.cs
@@ -5,8 +5,15 @@ public class CollectObject : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 10.0f;
     [SerializeField] private float collectionDelay = 2.0f;
+    [SerializeField] private AudioClip collectedAudioClip;
+    [SerializeField] private AudioSource audioSource;
     
     bool isGrabbing = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,6 +33,12 @@ public class CollectObject : MonoBehaviour
         isGrabbing = true;
         yield return new WaitForSeconds(collectionDelay);
 
+        if(!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(collectedAudioClip);
+        }
+
+        //TODO: SWAP deactivate with some other change
         gameObject.SetActive(false);
 
         isGrabbing = false;
