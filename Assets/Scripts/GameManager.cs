bb6394efeb349aa19e3dc7ada9f75ee0fb5dac08
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2026-03-05 22:00:38 -0800

    added timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.ShaderGraph.Internal;
 
 public class GameManager : MonoBehaviour
 {
 
-    TextMeshProUGUI timerText;
+    [Header("Gameplay Canvas Data")]
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] TextMeshProUGUI itemsCollected;
 
-    Canvas buttonCanvas;
-    Button startButton;
-    Button retryButton;
+    [Header("Button Canvas Data")]
+    [SerializeField] Canvas buttonCanvas;
+    [SerializeField] Button startButton;
+    [SerializeField] Button retryButton;
+
+    [TooltipAttribute("A value of 1 is equal to 1s. If you need 10s you input 10.")]
+    [SerializeField] private float timer = 180.0f;
+    private float countDown;
+
+    void Start()
+    {
+        countDown = timer;
+    }
 
     void Update()
     {
-        
+        ConvertToMMSS();
+    }
+
+    void ConvertToMMSS()
+    {
+        countDown -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(countDown / 60);
+        int seconds = Mathf.FloorToInt(countDown % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
